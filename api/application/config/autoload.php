63ebe1a13fc130ac5e1c30d3c029dd0854c2480f
Samuel Erwardi <samuelerwardi@gmail.com>
2018-12-10 22:54:53 +0700

    # fix autoload.php class indexfixer

--- api/application/config/autoload.php
+++ api/application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database','form_validation', 'format', 'indexfixer');
+$autoload['libraries'] = array('database','form_validation', 'format', 'IndexFixer');
 
 /*
 | -------------------------------------------------------------------
