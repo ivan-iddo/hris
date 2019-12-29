931f2a656354f209aa0812aa62736709cf36629e
yadyi putra <yadyiputra12@gmail.com>
2019-12-29 23:49:14 +0700

    ok

--- api/application/views/pegawai/view_str.php
+++ api/application/views/pegawai/view_str.php
@@ -4,8 +4,8 @@
     <tr>
         <td><?php echo $key + 1 ?></td>
         <td><?php echo $value["str"] ?></td>
-        <td><?php if(!empty($value["date_start"])){echo date_format(date_create($value["date_start"]), "d-m-Y")} ?></td>
-        <td><?php if(!empty($value["date_end"])){echo date_format(date_create($value["date_end"]), "d-m-Y")} ?></td>
+        <td><?php if(!empty($value["date_start"])){echo date_format(date_create($value["date_start"]), "d-m-Y");} ?></td>
+        <td><?php if(!empty($value["date_end"])){echo date_format(date_create($value["date_end"]), "d-m-Y");} ?></td>
         <td>
             <a title="Lihat File" id="book1-trigger" class="btn btn-default" href="javascript:void(0)" onclick="buildBook('api/upload/data/<?php echo $value["url"] ?>')">
               <i class="fa fa-eye"></i>
